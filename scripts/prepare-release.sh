a216e36849d760173bdc82e454b22d308d70db41
rjhallsted <rjhallsted@gmail.com>
2026-07-31 13:33:46 -0600

    Remove failing, unecessary line from release script

--- scripts/prepare-release.sh
+++ scripts/prepare-release.sh
@@ -51,7 +51,7 @@ cp -R node_modules .release/node_modules
 
 git checkout "$version"
 git pull
-git rm -rf node_modules
+# git rm -rf node_modules
 rm -rf node_modules  # remove node_modules/.cache
 
 rm -rf dist
