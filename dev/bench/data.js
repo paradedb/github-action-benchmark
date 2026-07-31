40347c107a67f110d8a9ed430363e02065986f58
github-action-benchmark <github@users.noreply.github.com>
2026-07-31 19:35:15 +0000

    add Catch2 Benchmark (v3) (catch2) benchmark result for a216e36849d760173bdc82e454b22d308d70db41

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1785526506430,
+  "lastUpdate": 1785526515839,
   "repoUrl": "https://github.com/paradedb/github-action-benchmark",
   "entries": {
     "Go Benchmark": [
@@ -592,6 +592,44 @@ window.BENCHMARK_DATA = {
             "range": "± 233.349",
             "unit": "us",
             "extra": "100 samples\n3 iterations"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "rjhallsted@gmail.com",
+            "name": "rjhallsted",
+            "username": "barbarj"
+          },
+          "committer": {
+            "email": "rjhallsted@gmail.com",
+            "name": "rjhallsted",
+            "username": "barbarj"
+          },
+          "distinct": true,
+          "id": "a216e36849d760173bdc82e454b22d308d70db41",
+          "message": "Remove failing, unecessary line from release script",
+          "timestamp": "2026-07-31T13:33:46-06:00",
+          "tree_id": "4e5bb8330e92097222df5303ea05f174b6e74a2f",
+          "url": "https://github.com/paradedb/github-action-benchmark/commit/a216e36849d760173bdc82e454b22d308d70db41"
+        },
+        "date": 1785526514164,
+        "tool": "catch2",
+        "benches": [
+          {
+            "name": "Fibonacci 10",
+            "value": 81.7451,
+            "range": "± 7.50663",
+            "unit": "ns",
+            "extra": "100 samples\n391 iterations"
+          },
+          {
+            "name": "Fibonacci 20",
+            "value": 7.48256,
+            "range": "± 402.531",
+            "unit": "us",
+            "extra": "100 samples\n5 iterations"
           }
         ]
       }
