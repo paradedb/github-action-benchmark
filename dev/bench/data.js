57f44b54784e13b98aad04b18df0acaea262e588
github-action-benchmark <github@users.noreply.github.com>
2026-07-31 15:11:26 +0000

    add Python Benchmark with pytest-benchmark (pytest) benchmark result for ddc9e59bfb2a35a1208c7c2bfae1962fc1c8f73f

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1785510678950,
+  "lastUpdate": 1785510686432,
   "repoUrl": "https://github.com/paradedb/github-action-benchmark",
   "entries": {
     "Go Benchmark": [
@@ -92,6 +92,46 @@ window.BENCHMARK_DATA = {
             "value": 18912.23,
             "range": "± 185.49",
             "unit": "ns/iter"
+          }
+        ]
+      }
+    ],
+    "Python Benchmark with pytest-benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "rjhallsted@gmail.com",
+            "name": "RJ Barman",
+            "username": "barbarj"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "ddc9e59bfb2a35a1208c7c2bfae1962fc1c8f73f",
+          "message": "Merge pull request #2 from paradedb/barbarj.clone-only-branch\n\nshallow-clone the target branch only",
+          "timestamp": "2026-07-31T11:10:54-04:00",
+          "tree_id": "f6e6db3c46b76eca309defac805a2983d066627f",
+          "url": "https://github.com/paradedb/github-action-benchmark/commit/ddc9e59bfb2a35a1208c7c2bfae1962fc1c8f73f"
+        },
+        "date": 1785510685791,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "bench.py::test_fib_10",
+            "value": 75585.91143796827,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0000012107644103207949",
+            "extra": "mean: 13.229978721903466 usec\nrounds: 42767"
+          },
+          {
+            "name": "bench.py::test_fib_20",
+            "value": 610.8706905337308,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00011011619742641755",
+            "extra": "mean: 1.6370076605349628 msec\nrounds: 598"
           }
         ]
       }
